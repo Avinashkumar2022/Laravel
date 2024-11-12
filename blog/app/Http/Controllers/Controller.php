de49af445419dd5999434457e6765753c3a28263
Avinash <avinashkumar200217@gmail.com>
2024-11-12 20:11:38 +0530

    controller

--- blog/app/Http/Controllers/Controller.php
+++ blog/app/Http/Controllers/Controller.php
@@ -4,5 +4,10 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    function getUser(){
+        return "Avinash and Anshu";
+    }
+    function aboutUser(){
+        return "Hello my name is Avinash";
+    }
 }
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,12 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::get('/abc', function () {
+Route::get('/', function () {
     return view('welcome');
 });
-// Route::get('/home', function () {
-//     return view('home');
-// });
+Route::get('/home', function () {
+    return view('home');
+});
 
-Route::view("/home",'home');
+// Route::view("/home",'home');
+
+Route::get('/about/{name}', function ($name) {
+    // echo $name;
+    return view('about',['name'=>$name]);
+});
+
+// Route::redirect('/home','/');   //Redirection
+
+Route ::get('user', [UserController::class,'getUser']);
+Route ::get('about', [UserController::class,'aboutUser']);
